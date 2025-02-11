3fca3afcedada1bc5785ebb59b3d5cfa322f2672
it-floothink <itfloothink@gmail.com>
2025-02-11 18:16:23 +0700

    adding function return_error_when_migrations_params_not_exist, and return_error_when_migrations_params_file_not_exist in CrudGeneratorCommandTest.php

--- tests/CrudGeneratorCommandTest.php
+++ tests/CrudGeneratorCommandTest.php
@@ -7,6 +7,7 @@ use Suryahadiningrat\CrudGenerator\CRUDGeneratorServiceProvider;
 class CrudGeneratorCommandTest extends TestCase
 {
     private $migrationPath = __DIR__ . '/stubs/example_migration.php';
+    private $wrongMigrationPath = __DIR__ . '/stubs/example_migration.sphp';
 
     protected function getPackageProviders($app)
     {
@@ -22,5 +23,21 @@ class CrudGeneratorCommandTest extends TestCase
             '--migration' => $this->migrationPath
         ])
         ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function return_error_when_migrations_params_not_exist()
+    {
+        $this->artisan('crud-generator:generate')
+        ->assertExitCode(1);
+    }
+
+    /** @test */
+    public function return_error_when_migrations_params_file_not_exist()
+    {
+        $this->artisan('crud-generator:generate', [
+            '--migration' => $this->wrongMigrationPath
+        ])
+        ->assertExitCode(1);
     }
 }
