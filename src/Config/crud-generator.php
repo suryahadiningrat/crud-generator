e389e6d06c5eb9231d514da0235d8d64b0fd57c4
it-floothink <itfloothink@gmail.com>
2025-02-12 13:20:17 +0700

    update crud-generator config and .gitignore

--- src/Config/crud-generator.php
+++ src/Config/crud-generator.php
@@ -2,6 +2,6 @@
 
 return [
     'model_namespace' => 'App\Models',
-    'model_path' => __DIR__.'/../../tests/stubs/tests',
-    'test_model_path' => __DIR__.'/../../tests/stubs/Models',
+    'model_path' => 'App\Models',
+    'test_model_path' => __DIR__.'/../../tests/stubs/Models', // For Testing Delete it
 ];
