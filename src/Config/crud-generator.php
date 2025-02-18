a96f51ac1c26de0dacc6ef114b2ac0cd597067ad
Suryahadiningrat <rifkysuryakusuma@floothink.com>
2025-02-18 17:05:14 +0700

    update config crud-generator and change directory stubs from resource to resources

--- src/Config/crud-generator.php
+++ src/Config/crud-generator.php
@@ -4,14 +4,14 @@ return [
     'model_namespace' => 'App\Models',
     'model_path' => 'app/Models',
     'request_path' => 'app/Http/Requests',
-    'resource_path' => 'app/Http/Resource',
+    'resource_path' => 'app/Http/Resources',
     'repository_path' => 'app/Http/Repository',
     'controller_path' => 'app/Http/Controllers',
 
     // For Testing Usage (Delete it)
     'test_model_path' => __DIR__.'/../../tests/stubs/Models',
     'test_request_path' => __DIR__.'/../../tests/stubs/Requests',
-    'test_resource_path' => __DIR__.'/../../tests/stubs/Resource',
+    'test_resource_path' => __DIR__.'/../../tests/stubs/Resources',
     'test_repository_path' => __DIR__.'/../../tests/stubs/Repository',
     'test_controller_path' => __DIR__.'/../../tests/stubs/Controller',
 ];
